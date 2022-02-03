876e36000e50aba94c40e00e2202a3fac5198b99
Niko Oksanen <niko.s.oksanen@gmail.com>
2022-02-03 16:20:02 +0200

    tehtavat 4.13-4.14

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -22,4 +22,27 @@ blogsRouter.post('/', async (request, response) => {
   response.json(savedBlog.toJSON())
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  if(!body.title || !body.url){
+    return response.status(400).end()
+  }
+
+  const blog = {
+    author: body.author,
+    title: body.title,
+    url: body.url,
+    likes: body.likes || 0
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
